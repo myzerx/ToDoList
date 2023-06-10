2b77c5d799606af5d35aae8029fe01f6bda81271
Richard Lemos <eternal.cassola@gmail.com>
2023-06-10 18:31:52 -0300

    wip(Home): send input task

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import { PlusCircle } from 'phosphor-react'
-
+import { useForm } from 'react-hook-form'
 import {
   CreateTaskButton,
   FlexInput,
@@ -9,15 +9,28 @@ import {
 import { Content } from '../../components/Content'
 
 export function Home() {
+  const { register, handleSubmit } = useForm()
+
+  function handleCreateNewTask(data: any) {
+    console.log(data)
+  }
+
   return (
     <HomeContainer>
-      <FlexInput>
-        <InputAddNewTask type="text" placeholder="Adicione uma nova tarefa" />
-        <CreateTaskButton type="submit">
-          Criar
-          <PlusCircle size={20} />
-        </CreateTaskButton>
-      </FlexInput>
+      <form onSubmit={handleSubmit(handleCreateNewTask)}>
+        <FlexInput>
+          <InputAddNewTask
+            id="task"
+            type="text"
+            placeholder="Adicione uma nova tarefa"
+            {...register('task')}
+          />
+          <CreateTaskButton type="submit">
+            Criar
+            <PlusCircle size={20} />
+          </CreateTaskButton>
+        </FlexInput>
+      </form>
       <Content />
     </HomeContainer>
   )
